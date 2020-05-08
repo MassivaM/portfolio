5e5d5699080b748aadae7cb813aa529c607518ea
MassivaM <hamlimassy@hotmail.com>
2020-05-08 19:03:52 -0400

    tech portion home page

--- massy-portfolio/src/Home.js
+++ massy-portfolio/src/Home.js
@@ -3,17 +3,26 @@ import {Route} from 'react-router-dom'
 import Navbar from './Navbar'
 import About from './About'
 import './Home.css'
+import logo from './kiss2.png';
+
 
 
 function Home () {
+
     return(
         <div>
             <Navbar />
             <Route exact path="/about"component={About}/>
                 <div className ="tech">
-                <div className = "title">
-                    <h1></h1>
-                    <h1> TECH PROJECTS</h1>
+                
+                <div className="title">
+                    <span> TECH <br></br>PROJECTS</span>
+                    <img src={logo} className="figure" />  
+                </div>
+                 <div className = "enter">
+                    
+
+                     Press k to view
                  </div>
                  </div>
             
